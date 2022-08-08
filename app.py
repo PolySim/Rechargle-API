88fdbd8190481fae68fc3d3686b4c9e21bd5beb9
Simon Desdevides <107926229+PolySim@users.noreply.github.com>
2022-08-08 14:23:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,53 @@
-import flask
-
-
+from flask import Flask, request, jsonify
 app = Flask(__name__)
-CORS(app, supports_credentials=True)
+
+
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from the url parameter /getmsg/?name=
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"Received: {name}")
+
+    response = {}
+
+    # Check if the user sent a name at all
+    if not name:
+        response["ERROR"] = "No name found. Please send a name."
+    # Check if the user entered a number
+    elif str(name).isdigit():
+        response["ERROR"] = "The name can't be numeric. Please send a string."
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome API!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    param = request.form.get('name')
+    print(param)
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome API!",
+            # Add this option to distinct the POST request
+            "METHOD": "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "No name found. Please send a name."
+        })
 
 
 @app.route('/')
 def index():
-    return "<h1>Hello World</h1>"
-
-@app.route('/hello')
-def say_hello_world():
-    return flask.jsonify({'result': "Hello Connected React World!!!"})
+    # A welcome message to test our server
+    return "<h1>Welcome to our medium-greeting-api!</h1>"
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=6789)
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
